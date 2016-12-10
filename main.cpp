c21c9c3fe65e165b25b40120244f48bb30a1e37b
Kartoshechka987 <ekaterina9214966670@outlook.com>
2016-12-10 22:38:54 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,41 +2,27 @@
 
 using namespace std;
 
-int a[10];
-void QuickSort(int l, int r)
-int x = a[(r+l)/2];
-int i = l;
-int j = r;
-while (i <= j)
-{
-while (a[i] < x)
-i++;
-while (a[j] > x)
-j++;
-if (i <= j)
-{
-    swap(a[i], a[j]);
-    i++;
-    j--;
-}
-}
-if(i<r)
-    QuickSort(i,r);
-    if (l<j)
-    QuickSort(l,j);
+int a, b, c, d, e, f, g, h, i, j, k, l;
 
 int main()
 {
-int n;
-cin >>n;
-for (int i = 0; i<n; i++)
-{
-   cin >> a[i];
-}
-QuickSort(0,n-1);
-for (int i = 0; i<n; i++)
-{
-    cout << a[i] << "";
-}
+    for (a = 0; a < 10; a++)
+        for (b = 0; b < 10; b++)
+        for (c = 0; c < 10; c++)
+        if ((a + b + c) % 2 == 0)
+        for (d = 0; d < 10; d++)
+        for (e = 0; e < 10; e++)
+        for (f = 0; f < 10; f++)
+        if (b + d + f == 21)
+        for (g = 0; g < 10; g++)
+        for (h = 0; h < 10; h++)
+       if (b + d + f + h == 21)
+       for (i = 0; i < 10; i++)
+       for (j = 0; j < 10; j++)
+       if (b + d + f + h + j == 21)
+       for (k = 0; k < 10; k++)
+       for (l = 0; l < 10; l++)
+       if (b + d + f + h + j + l == 21 && (j + k + l) % 3 == 0 && (c + f + i + l) % 11 == 0)
+       cout << a << b << c << d << e << f << g << h << i << j << k << l << endl;
     return 0;
 }
