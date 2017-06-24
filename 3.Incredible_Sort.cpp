104526b6430936156714c6ec14f7d505f1a379f8
Kartoshechka987 <ekaterina9214966670@outlook.com>
2017-06-24 19:35:52 +0300

    Update 3.Incredible_Sort.cpp

--- 3.Incredible_Sort.cpp
+++ 3.Incredible_Sort.cpp
@@ -1,28 +1,39 @@
 #include <iostream>
+
 using namespace std;
+
 void  incredible_sort(int n, int k)
 {
-	int *vsedok = new int[k];
-	for (int i = 0; i < k; i++)
-	{
-		vsedok[i] = 0;
-	}
+	int *a = new int[n];
+
 	for (int i = 0; i < n; i++)
 	{
 		int chislo;
 		cin >> chislo;
-		vsedok[chislo - 1] = vsedok[chislo - 1] + 1;
+		if(chislo <= k) a[i] = chislo; else a[i] = 0; //если элемент превышает к, то массив заполняется нулем
+
 	}
-	for (int i = k - 1; i >= 0; i--)
-	{
-		if (vsedok[i] != 0)
-		{
-			for (int j = 0; j < vsedok[i]; i++)
-			{
-				cout << i + 1 << " ";
-			}
-		}
-	}
+    for (int j=0; j<n; j++)
+    {
+        int num_max = j;  // номер максимального элемента
+        //обработка - поиск маскимального элемента в еще неупорядоченной части
+        for (int i=j; i<n; i++)
+        {
+            if (a[i]>a[num_max])
+            {
+                num_max=i;
+            }
+        }
+       int temp=a[j];
+        a[j]=a[num_max];
+        a[num_max]=temp;
+    }
+        for (int i=0; i<n; i++)
+        {
+            if (a[i] != 0)
+                cout << a[i]<<" ";
+        }
+
 }
 int main()
 {
