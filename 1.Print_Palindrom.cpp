07d83b9fb42e901cdae0dbcf5793236c49e05335
Kartoshechka987 <ekaterina9214966670@outlook.com>
2017-06-24 18:33:21 +0300

    Update 1.Print_Palindrom.cpp

--- 1.Print_Palindrom.cpp
+++ 1.Print_Palindrom.cpp
@@ -1,45 +1,38 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
-void perevernut(int x)
-{
-	while (x)
-	{
-		cout << x % 10;
-		x = x / 10;
-	}
-}
 void print_palindrom(int n)
 {
-	if (n % 2 == 0)
-	{
-		int i = pow(10, n / 2 - 1), chislo = i - 1;
-		while (true)
-		{
-			chislo = chislo + 9;
-			if (chislo / (10 * i) != 0) break;
-			cout << chislo;
-			perevernut(chislo);
-			cout << " ";
-		}
-	}
-	else
-	{
-		int i = pow(10, n / 2 - 1), chislo = i - 1;
-		while (true)
-		{
-			chislo = chislo + 1;
-			if (chislo / (10 * i) != 0) break;
-			cout << chislo << 9 - ((chislo * 2) % 9);
-			perevernut(chislo);
-			cout << " ";
-		}
-	}
+    int i, j, na, ko, otvet=0, k;
+    na=pow(10,(n-1)); // n-значные числа начинаются
+    ko=pow(10,n)-1; // n-значные числа завершаются
+    while (na<=ko+1)
+    {     // перебор n-значных чисел от начала до конца
+        j=n;
+        otvet=0;           // счетчик совпадений цифр в числе, симметричных относительно середины
+        for (i=1; i<=(n/2); i++)
+        {         // перебор с начального разряда до середины n-значного числа
+            if  ( ( (int) (na/pow(10,i-1)) %10 )  == ( (int) (na/pow(10,j-1)) %10 ) )
+            {  // если цифры совпали,
+                otvet++;  // то увеличить счетчик совпадений
+            }
+            j--;                          // перебор с конечного разряда до середины
+            if (otvet == n/2 && na%9 == 0)// если одна половина цифр в числе совпала с другой симметричной половиной и число делитля на 9,
+            {
+                 cout << na <<" ";        // то надо вывести число-палиндром на экран
+            }
+        }
+        na++;     // перебор - переход к следющеу числу
+    }
 }
-int main()
-{
-	int n;
-	cin >> n;
-	print_palindrom(n);
-	return 0;
+
+int main() {
+
+    int m;
+    cin >> m;
+    cout << endl;
+    print_palindrom(m); // вызов процедуры печати палиндрома
+    return 0;
 }
+
